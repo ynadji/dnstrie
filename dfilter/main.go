904755e41caf5a264ecd790f1346c285790a8fc4
Yacin Nadji <ynadji@securityscorecard.io>
2019-03-25 22:41:50 -0400

    add dfilter cmd

---
+++ dfilter/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/influxdata/influxdb/kit/cli"
+	"github.com/ynadji/dnstrie"
+)
+
+var root *dnstrie.DomainTrie
+
+var flags struct {
+	matchFile string
+	wildcard  bool
+}
+
+func readDomains(matchFilePath string) []string {
+	f, err := os.Open(matchFilePath)
+	if err != nil {
+	}
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+	return strings.Split(string(content), "\n")
+}
+
+func run() error {
+	// TODO: prob just switch to straight github.com/spf13/cobra
+	if flags.matchFile == "" {
+		fmt.Fprintln(os.Stderr, "Must provide match file!")
+		os.Exit(2)
+	}
+
+	domains := readDomains(flags.matchFile)
+	root = dnstrie.MakeTrie(domains)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		domain := scanner.Text()
+		matched := false
+		if flags.wildcard {
+			matched = root.WildcardMatch(domain)
+		} else {
+			matched = root.ExactMatch(domain)
+		}
+
+		if matched {
+			fmt.Printf("%v\n", domain)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("Error reading standard input: %+v\n", err)
+	}
+	return nil
+}
+
+func main() {
+	cmd := cli.NewCommand(&cli.Program{
+		Run:  run,
+		Name: "dfilter",
+		Opts: []cli.Opt{
+			{
+				DestP: &flags.matchFile,
+				Flag:  "matchFile",
+				Desc:  "File of domain matches, one per line",
+			},
+			{
+				DestP:   &flags.wildcard,
+				Flag:    "wildcard",
+				Default: false,
+				Desc:    "Accept wildcard matches",
+			},
+		},
+	})
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
